1fc6d1ba8402f8cc507c1afb3d96fa84a3d8bfff
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-09-05 12:09:42 -0400

    first commit

---
+++ src/test/java/glab/_953/ApplicationTests.java
@@ -0,0 +1,13 @@
+package glab._953;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
